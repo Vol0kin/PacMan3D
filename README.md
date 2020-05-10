abe3d4953fd45fae4b8188f72850e859e54df83e
Vladislav Nikolov <vladis890@gmail.com>
2020-05-10 20:24:19 +0200

    Added initial files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Pacman3D
+# PacMan3D
 Proyecto desarrollado para la asignatura de Sistemas Gráficos impartida en la Universidad de Granada durante el curso 2019/20.
---
+++ src/PacMan.js
@@ -0,0 +1,12 @@
+/**
+ * Clase que representa al PacMan.
+ */
+class PacMan extends THREE.Object3D {
+    constructor() {
+        super();
+    }
+
+    update() {
+        
+    }
+}
